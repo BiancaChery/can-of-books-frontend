37530624266bad53a9f2e9708feb16317183175e
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-08-17 02:02:01 +0000

    first commit

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import Profile from './About';
 
 class Header extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         {/* PLACEHOLDER: render a navigation link to the about page */}
+        <NavItem><Link to="/About" className="nav-link">About</Link></NavItem>
       </Navbar>
     )
   }
