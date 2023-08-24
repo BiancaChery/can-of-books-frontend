23fa581938d009da6afde7bf34cfe1ec873921bb
Gerard <gerard.g.johnson@gmail.com>
2023-08-23 21:18:30 -0500

    added github pages

--- src/About.js
+++ src/About.js
@@ -1,11 +1,42 @@
 import { Component } from "react";
 
 class Profile extends Component {
-
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    const developers = [
+      {
+        name: "Gerard Johnson",
+        github: "https://github.com/codexwithg",
+      },
+      {
+        name: "Bianca Chery",
+        github: "https://github.com/BiancaChery",
+      },
+      {
+        name: "Justine Oyaghiro",
+        github: "https://github.com/joyagh",
+      },
+      //Add more developers here
+    ];
+    return (
+      <div>
+        <h1>Developers</h1>
+        {delevopers.map((developer, index) => (
+          <div key={index}>
+            <h2>{developer.name}</h2>
+            <p>Profile page coming soon</p>
+            <a
+              href={developer.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github Profile
+            </a>
+          </div>
+        ))}
+      </div>
+    );
   }
-};
+}
 
 export default Profile;
