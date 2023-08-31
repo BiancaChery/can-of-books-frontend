cc71a1bd061ecc8e845796915f09606ebe47a793
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-08-31 01:39:51 +0000

    Added update form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./About";
+import { Link } from "react-router-dom";
+import Books from "./Books";
+
 
 class App extends React.Component {
   render() {
@@ -18,6 +21,17 @@ class App extends React.Component {
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
             <Route exact path="/about" element={<About />}></Route>
           </Routes>
+          <Form>
+            <h2>Book List</h2>
+            <ul>
+              {Books.map((book) => (
+                <li key={book.id}>
+                  {book.title} is {book.status}
+                  <Link to={'/edit/${book.id}'}>Edit</Link>
+                </li>
+              ))}
+            </ul>
+          </Form>
           <Footer />
         </Router>
       </>
